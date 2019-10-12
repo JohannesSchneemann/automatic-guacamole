ee51f8564ec395483626f20f120722d011865037
Johannes Schneemann <hannes.schneemann@gmail.com>
2019-10-11 21:14:58 -0500

    created product JS file

---
+++ products.js
@@ -0,0 +1,61 @@
+[{
+    "id":123,
+    "title":"alternator",
+    "desc":"Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    "img":"alternator.png",
+    "price":179.00
+},{
+    "id":456,
+    "title":"battery",
+    "desc":"Quisquam, veritatis, officia. Veritatis, saepe!",
+    "img":"battery.png",
+    "price":135.00
+},{
+    "id":789,
+    "title":"brake disk",
+    "desc":"Fugit dolorum consequatur rem molestiae, possimus dignissimos!",
+    "img":"brake_disk.png",
+    "price":30.00
+},{
+    "id":987,
+    "title":"drive belt",
+    "desc":"Donec arcu risus diam amet sit.",
+    "img":"drive_belt.png",
+    "price":10.00
+},{
+    "id":654,
+    "title":"engine oil",
+    "desc":"In vitae vel, wisi at, id praesent bibendum libero faucibus porta egestas, quisque praesent ipsum fermentum tempor.",
+    "img":"engine_oil.png",
+    "price":25.00
+},{
+    "id":321,
+    "title":"floor mats",
+    "desc":"Maecenas aliquam maecenas ligula nostra, accumsan taciti.",
+    "img":"floor_mats.png",
+    "price":89.00
+},{
+    "id":234,
+    "title":"light bulbs",
+    "desc":"Arcu habitasse elementum est, ipsum purus pede porttitor class.",
+    "img":"light_bulbs.png",
+    "price":2.00
+},{
+    "id":567,
+    "title":"spark plug",
+    "desc":"Porttitor mollis imperdiet libero senectus pulvinar.",
+    "img":"spark_plug.png",
+    "price":8.00
+},{
+    "id":809,
+    "title":"tires",
+    "desc":"Repellat orci erat et, sem cum, ultricies sollicitudin amet eleifend dolor nullam erat, malesuada est leo ac.",
+    "img":"tires.png",
+    "price":85.00
+},{
+    "id":101,
+    "title":"windshield wipers",
+    "desc":"Duis montes, tellus lobortis lacus amet arcu et.",
+    "img":"windshield_wipers.png",
+    "price":12.00
+}]
