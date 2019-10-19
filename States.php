623ad9d3f0126af27e4c53e50b2e63de84214e92
Johannes Schneemann <hannes.schneemann@gmail.com>
2019-10-19 16:28:57 -0500

    password

--- States.php
+++ States.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "Snow_man/85cs", "cs3320"); 
+$mysqli = new mysqli("localhost", "root", "password", "cs3320");
 if($mysqli->connect_error) {
 exit('Could not connect');
 }$sql = "SELECT stateID, stateCode, stateName FROM States WHERE stateID = ?"; $stmt = $mysqli->prepare($sql);
