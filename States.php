c300cce4cc4f318ab759aab3acd05e48918a11a8
Johannes Schneemann <hannes.schneemann@gmail.com>
2019-10-19 15:42:27 -0500

    created States.php file

---
+++ States.php
@@ -0,0 +1,18 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "password", "cs3320"); 
+if($mysqli->connect_error) {
+exit('Could not connect');
+}$sql = "SELECT stateID, stateCode, stateName FROM States WHERE stateID = ?"; $stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($stateID, $stateCode, $stateName); $stmt->fetch();
+$stmt->close();
+echo "<table>";
+echo "<tr>";
+echo "<th>stateID</th>"; echo "<td>" . $stateID . "</td>";
+echo "<th>stateCode</th>"; echo "<td>" . $stateCode . "</td>";
+echo "<th>stateName</th>"; echo "<td>" . $stateName . "</td>"; 
+echo "</table>";
+?>
+ 
