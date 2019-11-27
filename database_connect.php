caf6857394d09ca083f9571d418297dc13f9f430
Johannes Schneemann <hannes.schneemann@gmail.com>
2019-11-26 21:25:37 -0600

    login changes

--- database_connect.php
+++ database_connect.php
@@ -2,7 +2,7 @@
 function OpenCon() {
 	$dbhost = "localhost";
  	$dbuser = "root";
- 	$dbpass = "not";
+ 	$dbpass = "root";
  	$db = "cs3320";
 	
  	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
