c203089d21b6d75fd1106cf635ea895c21c57389
Johannes Schneemann <hannes.schneemann@gmail.com>
2019-11-27 14:45:13 -0600

    new login page & file changes

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:8889');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root');
-   define('DB_DATABASE', 'cs3320');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
--- login.php
+++ login.php
@@ -1,75 +1,41 @@
 <?php
-   include("config.php");
-   session_start();
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
-?>
-<html>
-   
-   <head>
-      <title>Login Page</title>
-      
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-				
-            <div style = "margin:30px">
-               
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
-               
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-					
-            </div>
-				
-         </div>
-			
-      </div>
 
-   </body>
-</html>
+$servername = "localhost:8889";
+$username = "root";
+$password ="root";
+$dbname ="cs3320";
+
+$uname = $_GET["username"];
+$pwd = $_GET["password"];
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+	die("connections failed:" . $conn->connect_error);
+}
+
+
+//check to see if username and password are in DB
+$sql = "SELECT * FROM userCredentials WHERE username = '$uname' AND password = '$pwd' ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+
+	//retrieve userID number
+	while($row = $result->fetch_assoc()){
+	    $user = $row['userID'];
+	}
+
+    //save userid in php session
+	session_start();
+	$_SESSION['login_user']= $user;  // Initializing Session with value of user
+	 echo '<script>window.location.href = "CS3320Project.html";</script>';
+
+}else {
+	echo '<script>alert("Username and/or Password incorrect. Please try again.") </script>';
+	echo '<script>window.location.href = "login.html";</script>';
+}
+
+mysqli_close($conn)
+
+?>
